70c207d629ad717bf89879d427a320778491d464
Paul P.H. Wilson <paul.wilson@wisc.edu>
2022-11-03 17:44:19 -0500

    avoid existing photos

--- make_student_list.py
+++ make_student_list.py
@@ -1,8 +1,10 @@
-import csv as csv
+import csv
 from pathlib import Path
+import glob
 
 with open('students.csv', newline='') as students:
     student_list = csv.reader(students)
     next(student_list)
     for student in student_list:
-        Path('photos/' + student[0] + '_' + student[1] + '_' + student[2] + '.txt').touch()
+        if len(glob.glob("photos/" + student[0] + '*')) < 1:
+            Path('photos/' + student[0] + '_' + student[1] + '_' + student[2] + '.txt').touch()
