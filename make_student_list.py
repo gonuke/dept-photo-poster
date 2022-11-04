5224c4e0cb460f45d3526739dcf4bb04b58e6b1f
Paul P.H. Wilson <paul.wilson@wisc.edu>
2022-11-04 08:42:32 -0500

    add diagnostic prints but commented out

--- make_student_list.py
+++ make_student_list.py
@@ -6,5 +6,9 @@ with open('students.csv', newline='') as students:
     student_list = csv.reader(students)
     next(student_list)
     for student in student_list:
+        # print(glob.glob("photos/" + student[0] + '*'))
         if len(glob.glob("photos/" + student[0] + '*')) < 1:
+            # print("adding: ", student)
             Path('photos/' + student[0] + '_' + student[1] + '_' + student[2] + '.txt').touch()
+        else:
+            pass # print("skipping: ", student)
